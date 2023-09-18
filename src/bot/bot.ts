7d99a07bf7efe68e826bf440f08f453714609d3a
JKapturkiewicz <kuba.kapek@gmail.com>
2023-09-18 17:45:39 +0200

    Add base config

--- src/bot/bot.ts
+++ src/bot/bot.ts
@@ -1,3 +1,34 @@
-export class Bot {
-  // Logic should be encapsulated in bot class body
+import puppeteer, { Browser, Page } from "puppeteer";
+
+export default class Bot {
+  _browser: Browser | null = null;
+  _page: Page | null = null;
+
+// Initialize the scrapper Bot
+  async init(): Promise<void> {
+    this._browser = await puppeteer.launch({ headless: false });
+    this._page = await this._browser.newPage();
+  }
+
+// test: get page title
+  async getTitle(url: string): Promise<string> {
+    if (!this._page) {
+			throw new Error('Page has not been initialized. Call init() method first.');
+		}
+		try {
+			await this._page.goto(url)
+		} catch (error) {
+			console.error('Navigation error: ', error)
+			throw new Error('An error occurred while navigating to the page.')
+		}
+    const title = await this._page.evaluate(() => document.title);
+    return title;
+  }
+
+// Close the browser
+  async close(): Promise<void> {
+    if (this._browser) {
+      await this._browser.close();
+    }
+  }
 }
