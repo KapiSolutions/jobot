348265cdb2a51e7b755b4718943a10a9603bbf4b
JKapturkiewicz <kuba.kapek@gmail.com>
2023-09-20 15:30:19 +0200

    Implement the:protocol scrapper

--- src/scripts/findOffers.ts
+++ src/scripts/findOffers.ts
@@ -1,14 +1,19 @@
 import getOptions from "./getOptions";
 import type { ScrapperOptions, JobOffer } from "../types/main";
 import ScrapperNoFluffJobs from "../bot/scrapper/scrapperNoFluffJobs";
+import ScrapperTheProtocol from "../bot/scrapper/scrapperTheProtocol";
 
 const findOffers = async () => {
   const options: ScrapperOptions = await getOptions();
   console.log(`Searching for ${options.searchValue} jobs with a limit of ${options.maxRecords} offers.`);
 
-  const scrapNoFluffJobs = new ScrapperNoFluffJobs(options);
-  const noFluffJobs: Array<JobOffer> = await scrapNoFluffJobs.getJobs();
+  const scrappNoFluffJobs = new ScrapperNoFluffJobs(options);
+  const noFluffJobs: Array<JobOffer> = await scrappNoFluffJobs.getJobs();
   console.log(noFluffJobs);
+
+  const scrappTheProtocol = new ScrapperTheProtocol(options);
+  const theProtocol: Array<JobOffer> = await scrappTheProtocol.getJobs();
+  console.log(theProtocol);
 };
 
 findOffers();
