1319d740c9c61a21ca26395b3823a877bb2de146
JKapturkiewicz <kuba.kapek@gmail.com>
2023-09-21 12:01:11 +0200

    Implement saveJson function and save scrapped data to json file

--- src/scripts/findOffers.ts
+++ src/scripts/findOffers.ts
@@ -2,18 +2,20 @@ import getOptions from "./getOptions";
 import type { ScrapperOptions, JobOffer } from "../types/main";
 import ScrapperNoFluffJobs from "../bot/scrapper/scrapperNoFluffJobs";
 import ScrapperTheProtocol from "../bot/scrapper/scrapperTheProtocol";
+import saveJson from "./saveJson";
 
-const findOffers = async () => {
+const findOffers = async (): Promise<void> => {
   const options: ScrapperOptions = await getOptions();
   console.log(`Searching for ${options.searchValue} jobs with a limit of ${options.maxRecords} offers.`);
 
   const scrappNoFluffJobs = new ScrapperNoFluffJobs(options);
   const noFluffJobs: Array<JobOffer> = await scrappNoFluffJobs.getJobs();
-  console.log(noFluffJobs);
 
   const scrappTheProtocol = new ScrapperTheProtocol(options);
   const theProtocol: Array<JobOffer> = await scrappTheProtocol.getJobs();
-  console.log(theProtocol);
+
+  const offers = [...noFluffJobs, ...theProtocol];
+  saveJson(offers, options);
 };
 
 findOffers();
