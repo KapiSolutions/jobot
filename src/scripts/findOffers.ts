85f13b713be9bd306e5e5944a66badab71937a81
JKapturkiewicz <kuba.kapek@gmail.com>
2023-09-22 14:24:48 +0200

    Define input props and export function

--- src/scripts/findOffers.ts
+++ src/scripts/findOffers.ts
@@ -1,23 +1,24 @@
-import getOptions from "./getOptions";
 import type { ScrapperOptions, JobOffer } from "../types/main";
 import ScrapperNoFluffJobs from "../bot/scrapper/scrapperNoFluffJobs";
 import ScrapperTheProtocol from "../bot/scrapper/scrapperTheProtocol";
 import saveJson from "./saveJson";
 import saveCsv from "./saveCsv";
 
-const findOffers = async (): Promise<void> => {
-  const options: ScrapperOptions = await getOptions();
-  console.log(`Searching for ${options.searchValue} jobs with a limit of ${options.maxRecords} offers.`);
-
+const findOffers = async (options: ScrapperOptions): Promise<void> => {
+  console.log(`👓 Searching for ${options.searchValue} jobs with a limit of ${options.maxRecords} offers per service.`);
+  
+  console.log("⚙️  Scrapping NoFluffJobs...")
   const scrappNoFluffJobs = new ScrapperNoFluffJobs(options);
   const noFluffJobs: Array<JobOffer> = await scrappNoFluffJobs.getJobs();
 
+  console.log("⚙️  Scrapping The:Protocol...")
   const scrappTheProtocol = new ScrapperTheProtocol(options);
   const theProtocol: Array<JobOffer> = await scrappTheProtocol.getJobs();
 
   const offers = [...noFluffJobs, ...theProtocol];
   saveJson(offers, options);
   saveCsv(offers, options);
+  console.log("✔️  Done!")
 };
 
-findOffers();
+export default findOffers
