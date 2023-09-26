e0d2d5e2c289ec7ec6beea077713aa72152b52fe
JKapturkiewicz <kuba.kapek@gmail.com>
2023-09-26 11:00:54 +0200

    Remove locate-chrome

--- src/bot/bot.ts
+++ src/bot/bot.ts
@@ -1,7 +1,6 @@
 import { Cluster } from "puppeteer-cluster";
 import puppeteer from "puppeteer-extra";
 import StealthPlugin from "puppeteer-extra-plugin-stealth";
-import locateChrome from "locate-chrome";
 require("dotenv").config();
 
 export default class Bot {
@@ -9,16 +8,14 @@ export default class Bot {
 
   async initCluster(maxConcurrency: number): Promise<void> {
     puppeteer.use(StealthPlugin());
-    const executablePath: string =
-      (await new Promise((resolve) => locateChrome((arg: any) => resolve(arg)))) ||
-      process.env.PUPPETEER_EXECUTABLE_PATH;
     this.cluster = await Cluster.launch({
       concurrency: Cluster.CONCURRENCY_PAGE,
       maxConcurrency: maxConcurrency,
       puppeteerOptions: {
         headless: "new",
         defaultViewport: null,
-        executablePath: process.env.NODE_ENV === "production" ? executablePath : puppeteer.executablePath(),
+        executablePath:
+          process.env.NODE_ENV === "production" ? process.env.PUPPETEER_EXECUTABLE_PATH : puppeteer.executablePath(),
         args: ["--disable-gpu", "--disable-setuid-sandbox", "--no-sandbox", "--no-zygote"],
       },
     });
