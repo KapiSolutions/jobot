2ca2677a5aa5397dfb5c24eb58d4169ee338462c
JKapturkiewicz <kuba.kapek@gmail.com>
2023-09-26 17:22:14 +0200

    Check if scrapping elements exists before processing

--- src/bot/scrapper/scrapperNoFluffJobs.ts
+++ src/bot/scrapper/scrapperNoFluffJobs.ts
@@ -58,37 +58,45 @@ export default class ScrapperNoFluffJobs extends Bot {
           this.cluster.execute(async ({ page }): Promise<void> => {
             await page.goto(url);
             await page.waitForSelector("article");
-
             const offer: JobOffer = await page.evaluate((url: string) => {
               let salaryFrom: string, salaryTo: string, currency: string, addedAt: string;
 
-              const title = document.querySelector("article h1").textContent.trim();
+              const title = document.querySelector("article h1")?.textContent.trim();
               const jobDescription = document.querySelector("#posting-description nfj-read-more")?.textContent.trim();
               const requirements = document
                 .querySelector('[data-cy-section="JobOffer_Requirements"]')
                 ?.textContent.trim();
               const description = jobDescription ? jobDescription : requirements;
-              const company = document.querySelector("#postingCompanyUrl").textContent.trim();
+              const company = document.querySelector("#postingCompanyUrl")?.textContent.trim();
 
               // Extract salary from the string(eg.: 12 000 – 18 000 PLN)
-              const salary = document.querySelector(".salary h4").textContent.trim();
-              if (salary.includes("–")) {
-                const [salaryPart1, salaryPart2] = salary.split("–").map((part) => part.trim());
-                salaryFrom = salaryPart1.trim().replace(" ","");
-                salaryTo = salaryPart2.slice(0, salaryPart2.length - 3).trim().replace(" ","");
-                currency = salaryPart2.slice(salaryPart2.length - 3);
-              } else {
-                salaryFrom = salary.slice(0, salary.length - 3).trim().replace(" ","");
-                salaryTo = salaryFrom;
-                currency = salary.slice(salary.length - 3);
+              const salary = document.querySelector(".salary h4")?.textContent.trim();
+              if (salary) {
+                if (salary.includes("–")) {
+                  const [salaryPart1, salaryPart2] = salary?.split("–").map((part) => part.trim());
+                  salaryFrom = salaryPart1.trim().replace(" ", "");
+                  salaryTo = salaryPart2
+                    .slice(0, salaryPart2.length - 3)
+                    .trim()
+                    .replace(" ", "");
+                  currency = salaryPart2.slice(salaryPart2.length - 3);
+                } else {
+                  salaryFrom = salary
+                    .slice(0, salary.length - 3)
+                    .trim()
+                    .replace(" ", "");
+                  salaryTo = salaryFrom;
+                  currency = salary.slice(salary.length - 3);
+                }
               }
+
               // Extract required technologies
               const technologies = Array.from(document.querySelectorAll("#posting-requirements li")).map((item) =>
                 item.textContent.trim().replace("\n", "")
               );
               // Extract the job posting date
-              const postingMsg = document.querySelector(".posting-time-row").textContent.trim();
-              const numberFound = postingMsg.match(/\d+/);
+              const postingMsg = document.querySelector(".posting-time-row")?.textContent.trim();
+              const numberFound = postingMsg?.match(/\d+/);
               if (numberFound) {
                 const today = new Date();
                 today.setDate(today.getDate() - parseInt(numberFound[0]));
@@ -99,7 +107,7 @@ export default class ScrapperNoFluffJobs extends Bot {
 
               return {
                 title,
-                description: description.replaceAll("\n"," "),
+                description: description.replaceAll("\n", " "),
                 company,
                 salaryFrom,
                 salaryTo,
