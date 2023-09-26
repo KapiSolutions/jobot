9b11940ace346846614adc194ebff7c2374bfab6
JKapturkiewicz <kuba.kapek@gmail.com>
2023-09-26 19:37:38 +0200

    Scrapp theProtocol first

--- src/scripts/findOffers.ts
+++ src/scripts/findOffers.ts
@@ -4,22 +4,16 @@ import ScrapperTheProtocol from "../bot/scrapper/scrapperTheProtocol";
 import saveJson from "./saveJson";
 import saveCsv from "./saveCsv";
 
-function sleep(ms: number) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
 const findOffers = async (options: ScrapperOptions): Promise<Array<JobOffer>> => {
   console.log(`👓 Searching for ${options.searchValue} jobs with a limit of ${options.maxRecords} offers per service.`);
+  
+  console.log("⚙️  Scrapping The:Protocol...");
+  const scrappTheProtocol = new ScrapperTheProtocol(options);
+  const theProtocol: Array<JobOffer> = await scrappTheProtocol.getJobs();
 
   console.log("⚙️  Scrapping NoFluffJobs...");
   const scrappNoFluffJobs = new ScrapperNoFluffJobs(options);
   const noFluffJobs: Array<JobOffer> = await scrappNoFluffJobs.getJobs();
-
-  await sleep(1000);
-
-  console.log("⚙️  Scrapping The:Protocol...");
-  const scrappTheProtocol = new ScrapperTheProtocol(options);
-  const theProtocol: Array<JobOffer> = await scrappTheProtocol.getJobs();
 
   const offers = [...noFluffJobs, ...theProtocol];
 
