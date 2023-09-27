9fd95443986748d2f02b55a7cc5b0d759eac77e6
JKapturkiewicz <kuba.kapek@gmail.com>
2023-09-27 11:17:24 +0200

    Implement pracuj.pl scrapper and disable the:protocol scrapper

--- src/scripts/findOffers.ts
+++ src/scripts/findOffers.ts
@@ -1,21 +1,27 @@
 import type { ScrapperOptions, JobOffer } from "../types/main";
 import ScrapperNoFluffJobs from "../bot/scrapper/scrapperNoFluffJobs";
-import ScrapperTheProtocol from "../bot/scrapper/scrapperTheProtocol";
+// import ScrapperTheProtocol from "../bot/scrapper/scrapperTheProtocol";
+import ScrapperPracujPl from "../bot/scrapper/scrapperPracujPL";
 import saveJson from "./saveJson";
 import saveCsv from "./saveCsv";
 
 const findOffers = async (options: ScrapperOptions): Promise<Array<JobOffer>> => {
   console.log(`👓 Searching for ${options.searchValue} jobs with a limit of ${options.maxRecords} offers per service.`);
   
-  console.log("⚙️  Scrapping The:Protocol...");
-  const scrappTheProtocol = new ScrapperTheProtocol(options);
-  const theProtocol: Array<JobOffer> = await scrappTheProtocol.getJobs();
+  // !The:Protocol scrapping disabled: cloudflare blocks bot on the production(render.com)
+  // console.log("⚙️  Scrapping The:Protocol...");
+  // const scrappTheProtocol = new ScrapperTheProtocol(options);
+  // const theProtocol: Array<JobOffer> = await scrappTheProtocol.getJobs();
 
   console.log("⚙️  Scrapping NoFluffJobs...");
   const scrappNoFluffJobs = new ScrapperNoFluffJobs(options);
   const noFluffJobs: Array<JobOffer> = await scrappNoFluffJobs.getJobs();
 
-  const offers = [...noFluffJobs, ...theProtocol];
+  console.log("⚙️  Scrapping Pracuj.pl...");
+  const scrappPracujPl = new ScrapperPracujPl(options);
+  const pracujPl: Array<JobOffer> = await scrappPracujPl.getJobs();
+
+  const offers = [...noFluffJobs, ...pracujPl];
 
   if (false) {
     // disable saving data
